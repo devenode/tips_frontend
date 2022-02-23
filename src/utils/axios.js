a6deabaa62474b8a83117549e0aa1bfcc77554e1
Nastromo <nastromo@solaris-2.local>
2022-02-23 17:54:51 +0200

    Deploy setup

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const req = axios.create({
-   baseURL: process.env.NODE_ENV === `development` ? `http://localhost:4000` : `https://tips.ga`,
+   baseURL: process.env.NODE_ENV === `development` ? `http://localhost:4000` : `http://tipz.ga`,
    timeout: 4000,
 });
 
